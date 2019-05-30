97a70d74722ca92fa1d3b9a1962dd9df27cd4325
Alex Heist <dev@alexheist.com>
2019-05-29 19:54:50 -0600

    git clone from remote success

--- app/website/repotool/management/commands/add_repo.py
+++ app/website/repotool/management/commands/add_repo.py
@@ -1,9 +1,10 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
-from pygit2 import clone_repository
+import pygit2
 
 class Command(BaseCommand):
 	help = 'Adds a repository into the system'
+	credentials = pygit2.UserPass(os.environ.get('GIT_USER'), os.environ.get('GIT_PASS'))
 
 	def add_arguments(self, parser):
 		parser.add_argument('urls', nargs='+', type=str)
@@ -12,7 +13,8 @@ class Command(BaseCommand):
 		for url in options['urls']:
 			try:
 				path = '{}/{}'.format(settings.REPOSITORY_DIR, url.split('/')[-1])
-				clone_repository(url, path)
+				callbacks = pygit2.RemoteCallbacks(self.credentials)
+				pygit2.clone_repository(url, path, callbacks=callbacks)
 				self.stdout.write(self.style.SUCCESS('Success'))
 			except Exception as e:
 				self.stdout.write(e)
