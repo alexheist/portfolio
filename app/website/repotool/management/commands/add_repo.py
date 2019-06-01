658072034bca2bc559f10072d30718a0b6f3e068
Alex Heist <dev@alexheist.com>
2019-05-31 20:59:09 -0600

    git user/password provided in command, not env

--- app/website/repotool/management/commands/add_repo.py
+++ app/website/repotool/management/commands/add_repo.py
@@ -4,16 +4,25 @@ import pygit2
 
 class Command(BaseCommand):
 	help = 'Adds a repository into the system'
-	credentials = pygit2.UserPass(os.environ.get('GIT_USER'), os.environ.get('GIT_PASS'))
 
 	def add_arguments(self, parser):
-		parser.add_argument('urls', nargs='+', type=str)
+		parser.add_argument('--urls', nargs='+', type=str)
+		parser.add_argument('--user', type=str)
+		parser.add_argument('--password', type=str)
 
 	def handle(self, *args, **options):
+		try:
+			user = options['user']
+			password = options['password']
+			credentials = pygit2.UserPass(user, password)
+		except Exception as e:
+			self.stdout.write(e)
+			self.stdout.write(self.style.ERROR('Something went wrong'))
+
 		for url in options['urls']:
 			try:
 				path = '{}/{}'.format(settings.REPOSITORY_DIR, url.split('/')[-1])
-				callbacks = pygit2.RemoteCallbacks(self.credentials)
+				callbacks = pygit2.RemoteCallbacks(credentials)
 				pygit2.clone_repository(url, path, callbacks=callbacks)
 				self.stdout.write(self.style.SUCCESS('Success'))
 			except Exception as e:
