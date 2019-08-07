fccd77deb7ade9256c97a8710ce38a75c102bced
Alex Heist <dev@alexheist.com>
2019-08-06 22:02:24 -0600

    Add components & styling - Create basic style of website - WIP, needs more css

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -15,10 +15,12 @@ function Nav() {
 function Hero() {
     return (
         <div id="hero">
-            <h1>Alex Heist</h1>
-            <p>Software Developer</p>
-            <a class="clear-btn" href="https://github.com/alexheist">GitHub</a>
-            <a class="clear-btn" href="https://linkedin.com/alexander-heist">LinkedIn</a>
+            <div id="hero-content">
+                <h1>Alex Heist</h1>
+                <p>Software Developer</p>
+                <a class="clear-btn" href="https://github.com/alexheist">GitHub</a>
+                <a class="clear-btn" href="https://linkedin.com/alexander-heist">LinkedIn</a>
+            </div>
         </div>
     )
 }
@@ -31,13 +33,54 @@ function Elevator() {
     )
 }
 
+function Projects() {
+    return (
+        <div id="projects">
+            <h2>My Projects</h2>
+            <img src="https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjFivTV4-jAhUiGTQIHb1xAQQQjRx6BAgBEAQ&url=https%3A%2F%2Floremipsum.io%2F21-of-the-best-placeholder-image-generators%2F&psig=AOvVaw3NxYpHwZCO1CVtCW9SBo3O&ust=1565233589685635" />
+            <h5>Project Name</h5>
+            <p>Some information about the project that I came up with on a whim</p>
+        </div>
+    )
+}
+
+function Contact() {
+    return (
+        <div id="contact">
+            <h2>Contact Me</h2>
+            <form method="post" action="">
+                <label for="name">Name</label>
+                <input type="text" id="name" name="name" />
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" />
+                <label for="message">Message</label>
+                <textarea name="message" id="message" rows="10" cols="50" />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <p>Alex Heist</p>
+        </footer>
+    )
+}
+
 class App extends React.Component {
     render() {
         return (
             <div>
                 <Nav />
                 <Hero />
-                <Elevator />
+                <div id="main-content">
+                    <Elevator />
+                    <Projects />
+                    <Contact />
+                </div>
+                <Footer />
             </div>
         )
     }
