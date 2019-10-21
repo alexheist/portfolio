54139ae573aa711bdca0cac9f0b88dea2a8e1bd7
Alex Heist <dev@alexheist.com>
2019-10-20 19:28:02 -0600

    Minor update before 2.0.1 - Added noopener & noreferrer to target=_blank links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Nav() {
     return (
         <nav>
-            <a href="https://github.com/alexheist" target="_blank">GitHub</a>
-            <a href="https://linkedin.com/in/alexander-heist" target="_blank">LinkedIn</a>
+            <a href="https://github.com/alexheist" target="_blank" rel="noopener noreferrer">GitHub</a>
+            <a href="https://linkedin.com/in/alexander-heist" target="_blank" rel="noopener noreferrer">LinkedIn</a>
         </nav>
     )
 }
