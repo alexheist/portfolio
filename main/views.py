50d0072088bdc20abcf399f06e816ee6aba052b5
Alex Heist <a.heist@alexheist.com>
2021-03-07 12:22:57 -0700

    base layout

--- main/views.py
+++ main/views.py
@@ -6,11 +6,18 @@ from . import forms, models
 
 
 def index(request):
-    popular, recent = blog_models.Article.get_preview_articles()
+    # popular, recent = blog_models.Article.get_preview_articles()
     if request.is_ajax():
         form = forms.LeadForm(request.POST)
         if form.is_valid():
             form.save()
             return http.HttpResponse(status=200)
         return http.HttpResponse(status=400)
-    return render(request, "main/index.html", {"popular": popular, "recent": recent})
+    return render(
+        request,
+        "main/index.html",
+        {
+            # "popular": popular,
+            # "recent": recent,
+        },
+    )
